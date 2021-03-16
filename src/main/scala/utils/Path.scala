818b782e3ffc925d6c12fe22003c7a8573ae2e5f
mlobo1997 <miguel.leite@blip.pt>
2021-03-16 17:29:42 +0000

    Make Path support relative paths

--- src/main/scala/utils/Path.scala
+++ src/main/scala/utils/Path.scala
@@ -1,42 +1,57 @@
 package com.mlobo
 package utils
 
-import utils.Path.SEPARATOR
+import utils.Path.{CURRENT_PATH, PARENT_PATH, ROOT_PATH, SEPARATOR}
 
-class Path(val listedPath: List[String], val basePath: Char) {
+import scala.annotation.tailrec
+import scala.util.matching.Regex
+
+class Path(val listedPath: List[String], val base: String) {
   def join(path: String): Path = {
     val newListedPath: List[String] =
       if (path.nonEmpty) listedPath :+ path else listedPath
-    new Path(newListedPath, basePath)
+    new Path(newListedPath, base)
   }
 
-  def getParent: Path = Path(listedPath.init, basePath)
+  def getParent: Path = Path(listedPath.init, base)
 
   def getLast: String = listedPath.last
 
+  def getAbsolutePath: Path = {
+    assert(base == ROOT_PATH)
+    @tailrec
+    def aux(newPath: List[String], next: List[String]): List[String] =
+      next match {
+        case Nil => newPath
+        case ::(head, tail) =>
+          head match {
+            case PARENT_PATH  => aux(newPath.init, tail)
+            case CURRENT_PATH => aux(newPath, tail)
+            case _            => aux(newPath :+ head, tail)
+          }
+      }
+    Path(aux(List(), listedPath), base)
+  }
+
   override def toString: String =
-    listedPath.foldLeft(basePath.toString)((a, b) =>
-      if (a != SEPARATOR.toString) a + SEPARATOR + b else a + b
+    listedPath.foldLeft(base)((a, b) =>
+      if (a != SEPARATOR) a + SEPARATOR + b else a + b
     )
 }
 
 object Path {
-  val CURRENT_PATH = '.'
-  val ROOT_PATH = '/'
-  val SEPARATOR = '/'
+  val ROOT_PATH = "/"
+  val CURRENT_PATH = "."
+  val PARENT_PATH = ".."
+  val SEPARATOR = "/"
+  val ROOT_PATTERN: Regex = s"$ROOT_PATH.*".r
 
-  def apply(listedPath: List[String], basePath: Char): Path =
+  def apply(listedPath: List[String], basePath: String): Path =
     new Path(listedPath, basePath)
 
   def apply(string: String): Path = {
-    val basePath: Char = string.headOption match {
-      case Some(char) =>
-        char match {
-          case ROOT_PATH => ROOT_PATH
-          case _         => CURRENT_PATH
-        }
-      case None => CURRENT_PATH
-    }
+    val basePath: String =
+      if (ROOT_PATTERN.matches(string)) ROOT_PATH else CURRENT_PATH
     Path(string.split(SEPARATOR).filter(_.nonEmpty).toList, basePath)
   }
 
--- src/test/scala/utils/PathTest.scala
+++ src/test/scala/utils/PathTest.scala
@@ -8,26 +8,46 @@ class PathTest extends AnyFunSuite {
     assert(Path("").listedPath equals List())
 
     val p1 = Path("/")
-    assert(p1.listedPath.equals(List()) && p1.basePath == '/')
+    assert(p1.listedPath.equals(List()) && p1.base == "/")
 
     val p2 = Path("/test")
-    assert(p2.listedPath.equals(List("test")) && p2.basePath == '/')
+    assert(p2.listedPath.equals(List("test")) && p2.base == "/")
 
     val p3 = Path("/test/t2")
-    assert(p3.listedPath.equals(List("test", "t2")) && p3.basePath == '/')
+    assert(p3.listedPath.equals(List("test", "t2")) && p3.base == "/")
 
     val p4 = Path("test/t2")
-    assert(p4.listedPath.equals(List("test", "t2")) && p4.basePath == '.')
+    assert(p4.listedPath.equals(List("test", "t2")) && p4.base == ".")
   }
 
   test("path.join") {
     val p1 = Path("/test").join("coiso")
-    assert(p1.basePath == '/' && p1.listedPath.equals(List("test", "coiso")))
+    assert(p1.base == "/" && p1.listedPath.equals(List("test", "coiso")))
   }
 
   test("path.toString") {
     assert(Path("/test/coiso").toString == "/test/coiso")
     assert(Path("test/coiso").toString == "./test/coiso")
     assert(Path("").toString == ".")
+  }
+
+  test("test.absolutePath") {
+    assert(
+      Path("/coiso/../test/test1").getAbsolutePath.toString == "/test/test1"
+    )
+
+    assert(
+      Path("/coiso/../test/../test1").getAbsolutePath.toString == "/test1"
+    )
+
+    assert(
+      Path("/coiso/test/../../test1").getAbsolutePath.toString == "/test1"
+    )
+
+    assert(
+      Path(
+        "/coiso/test/./test1"
+      ).getAbsolutePath.toString == "/coiso/test/test1"
+    )
   }
 }
